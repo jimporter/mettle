e0e2464920a80b7840ea4fedb24a1ff2846d658d
Jim Porter <jporter@mozilla.com>
2014-03-23 21:12:38 -0500

    Improve how suites are built by adding a suite_builder helper class. Now, the suite class itself just handles some simple delegation to the important parts

--- include/mettle/suite.hpp
+++ include/mettle/suite.hpp
@@ -3,6 +3,7 @@
 
 #include <algorithm>
 #include <functional>
+#include <memory>
 #include <string>
 #include <utility>
 #include <vector>
@@ -38,11 +39,11 @@ namespace detail {
   }
 }
 
-class suite_base;
-using suites_list = std::vector<suite_base*>;
+class runnable_suite;
+using suites_list = std::vector<std::shared_ptr<runnable_suite>>;
 suites_list all_suites;
 
-class suite_base {
+class runnable_suite {
 public:
   struct test_result {
     bool passed;
@@ -63,13 +64,12 @@ public:
 
   using iterator = std::vector<test_info>::const_iterator;
 
-  suite_base(const std::string &name, suites_list &suites = all_suites)
-    : name_(name), suites_(suites) {
-    suites_.push_back(this);
-  }
-
-  virtual ~suite_base() {
-    suites_.erase(std::find(suites_.begin(), suites_.end(), this));
+  template<typename T, typename Func>
+  runnable_suite(const std::string &name, const T &begin, const T &end,
+                 const Func &f) : name_(name) {
+    for(auto i = begin; i != end; ++i) {
+      tests_.push_back(f(*i));
+    }
   }
 
   iterator begin() const {
@@ -87,82 +87,100 @@ public:
   size_t size() const {
     return tests_.size();
   }
-protected:
+private:
   std::string name_;
   std::vector<test_info> tests_;
-  suites_list &suites_;
 };
 
 template<typename Exception, typename ...T>
-class basic_suite : public suite_base {
+class basic_suite {
 public:
-  using exception_type = Exception;
-  using function_type = std::function<void(T&...)>;
+  class suite_builder {
+  public:
+    using exception_type = Exception;
+    using function_type = std::function<void(T&...)>;
 
-  basic_suite(const std::string &name,
-              const std::function<void(basic_suite &)> &f,
-              suites_list &suites = all_suites) : suite_base(name, suites) {
-    initializing_ = true;
-    f(*this);
-    initializing_ = false;
-  }
+    suite_builder(const std::string &name) : name_(name) {}
+    suite_builder(const suite_builder &) = delete;
+    suite_builder & operator =(const suite_builder &) = delete;
 
-  void setup(const function_type &f) {
-    must_be_initializing(__func__);
-    setup_ = f;
-  }
-
-  void teardown(const function_type &f) {
-    must_be_initializing(__func__);
-    teardown_ = f;
-  }
-
-  void skip_test(const std::string &name, const function_type &f) {
-    must_be_initializing(__func__);
-    add_test(name, f, true);
-  }
-
-  void test(const std::string &name, const function_type &f) {
-    must_be_initializing(__func__);
-    add_test(name, f, false);
-  }
-private:
-  void must_be_initializing(const std::string &func) {
-    if(!initializing_) {
-      throw std::runtime_error(
-        "can only call " + func + "() while initializing");
+    void setup(const function_type &f) {
+      setup_ = f;
     }
-  }
 
-  void add_test(const std::string &name, const function_type &f, bool skip) {
-    test_info::function_type test_function = [f, this]() -> test_result {
-      std::tuple<T...> fixtures;
+    void teardown(const function_type &f) {
+      teardown_ = f;
+    }
 
-      bool passed = false;
-      std::string message;
-      try {
-        if(setup_)
-          detail::apply(setup_, fixtures);
-        detail::apply(f, fixtures);
-        if(teardown_)
-          detail::apply(teardown_, fixtures);
-        passed = true;
-      }
-      catch (const exception_type &e) {
-        message = e.what();
-      }
-      catch(...) {
-        message = "unknown error";
-      }
+    void skip_test(const std::string &name, const function_type &f) {
+      tests_.push_back({name, f, true});
+    }
 
-      return { passed, message };
+    void test(const std::string &name, const function_type &f) {
+      tests_.push_back({name, f, false});
+    }
+
+    std::shared_ptr<runnable_suite> finalize() {
+      using namespace std::placeholders;
+      return std::make_shared<runnable_suite>(
+        name_, tests_.begin(), tests_.end(),
+        std::bind(&suite_builder::make_test, this, _1)
+      );
+    }
+
+  private:
+    struct test_info {
+      std::string name;
+      function_type f;
+      bool skip;
     };
 
-    tests_.push_back({ name, test_function, skip });
-  }
+    runnable_suite::test_info make_test(const test_info &test) {
+      auto &f = test.f;
+      auto &setup = setup_;
+      auto &teardown = teardown_;
 
-  function_type setup_, teardown_;
-  bool initializing_;
+      runnable_suite::test_info::function_type test_function = [
+        f, setup, teardown
+      ]() -> runnable_suite::test_result {
+        bool passed = false;
+        std::string message;
+
+        try {
+          std::tuple<T...> fixtures;
+          if(setup)
+            detail::apply(setup, fixtures);
+          detail::apply(f, fixtures);
+          if(teardown)
+            detail::apply(teardown, fixtures);
+          passed = true;
+        }
+        catch (const exception_type &e) {
+          message = e.what();
+        }
+        catch(...) {
+          message = "unknown error";
+        }
+
+        return { passed, message };
+      };
+
+      return { test.name, test_function, test.skip };
+    }
+
+    std::string name_;
+    function_type setup_, teardown_;
+    std::vector<test_info> tests_;
+  };
+
+  basic_suite(const std::string &name,
+              const std::function<void(suite_builder &)> &f,
+              suites_list &suites = all_suites) {
+    suite_builder builder(name);
+    f(builder);
+    auto b = builder.finalize();
+    suites.push_back(b);
+  }
 };
 
 template<typename ...T>
--- test/test_suite.cpp
+++ test/test_suite.cpp
@@ -6,7 +6,7 @@ inline auto match_test(const std::string &name, bool skip) {
   if(skip)
     s << "skipped ";
   s << "test named \"" << name << "\"";
-  return make_matcher([name, skip](const suite_base::test_info &actual) {
+  return make_matcher([name, skip](const runnable_suite::test_info &actual) {
     return actual.name == name && actual.skip == skip;
   }, s.str());
 }
@@ -14,12 +14,14 @@ inline auto match_test(const std::string &name, bool skip) {
 suite<suites_list> test_suite("suite creation", [](auto &_) {
 
   _.test("create a test suite", [](suites_list &suites) {
-    suite<> inner("inner test suite", [](auto &_){
+    suite<>("inner test suite", [](auto &_){
       _.test("inner test", []() {});
       _.skip_test("skipped test", []() {});
     }, suites);
 
-    expect(suites, array(&inner));
+    expect(suites.size(), equal_to<size_t>(1));
+
+    auto &inner = *suites[0];
     expect(inner.size(), equal_to<size_t>(2));
     expect(inner, array(
       match_test("inner test", false), match_test("skipped test", true)
@@ -27,12 +29,14 @@ suite<suites_list> test_suite("suite creation", [](auto &_) {
   });
 
   _.test("create a test suite with fixture", [](suites_list &suites) {
-    suite<int> inner("inner test suite", [](auto &_){
+    suite<int>("inner test suite", [](auto &_){
       _.test("inner test", [](int &) {});
       _.skip_test("skipped test", [](int &) {});
     }, suites);
 
-    expect(suites, array(&inner));
+    expect(suites.size(), equal_to<size_t>(1));
+
+    auto &inner = *suites[0];
     expect(inner.size(), equal_to<size_t>(2));
     expect(inner, array(
       match_test("inner test", false), match_test("skipped test", true)
@@ -40,14 +44,16 @@ suite<suites_list> test_suite("suite creation", [](auto &_) {
   });
 
   _.test("create a test suite with setup/teardown", [](suites_list &suites) {
-    suite<> inner("inner test suite", [](auto &_){
+    suite<>("inner test suite", [](auto &_){
       _.setup([]() {});
       _.teardown([]() {});
       _.test("inner test", []() {});
       _.skip_test("skipped test", []() {});
     }, suites);
 
-    expect(suites, array(&inner));
+    expect(suites.size(), equal_to<size_t>(1));
+
+    auto &inner = *suites[0];
     expect(inner.size(), equal_to<size_t>(2));
     expect(inner, array(
       match_test("inner test", false), match_test("skipped test", true)
@@ -56,14 +62,16 @@ suite<suites_list> test_suite("suite creation", [](auto &_) {
 
   _.test("create a test suite with fixture and setup/teardown",
          [](suites_list &suites) {
-    suite<int> inner("inner test suite", [](auto &_){
+    suite<int>("inner test suite", [](auto &_){
       _.setup([](int &) {});
       _.teardown([](int &) {});
       _.test("inner test", [](int &) {});
       _.skip_test("skipped test", [](int &) {});
     }, suites);
 
-    expect(suites, array(&inner));
+    expect(suites.size(), equal_to<size_t>(1));
+
+    auto &inner = *suites[0];
     expect(inner.size(), equal_to<size_t>(2));
     expect(inner, array(
       match_test("inner test", false), match_test("skipped test", true)
@@ -72,12 +80,12 @@ suite<suites_list> test_suite("suite creation", [](auto &_) {
 
   _.test("create a test suite that throws", [](suites_list &suites) {
     try {
-      suite<int> inner("broken test suite", [](auto &){
+      suite<int>("broken test suite", [](auto &){
         throw "bad";
       }, suites);
     } catch(...) {}
 
-    expect(suites, array());
+    expect(suites.size(), equal_to<size_t>(0));
   });
 
 });
