344ce02e5734ec35787f1e8484cb86a8521e4885
Jim Porter <jporter@mozilla.com>
2014-03-28 01:45:38 -0500

    Show the full test suite hierarchy during failures

--- include/mettle/runner.hpp
+++ include/mettle/runner.hpp
@@ -8,20 +8,103 @@
 
 namespace mettle {
 
+using suites_list = std::vector<runnable_suite>;
+
+namespace detail {
+  suites_list all_suites;
+
+  struct test_results {
+    test_results() : passes(0), skips(0), total(0) {}
+
+    struct failure {
+      std::vector<std::string> suites;
+      std::string test, message;
+    };
+    std::vector<failure> failures;
+    size_t passes, skips, total;
+  };
+
+  template<typename T>
+  std::string join(T begin, T end, const std::string &delim) {
+    if(begin == end)
+      return "";
+
+    std::stringstream s;
+    s << *begin;
+    for(++begin; begin != end; ++begin)
+      s << delim << *begin;
+    return s.str();
+  }
+
+  void run_tests_impl(test_results &results, const suites_list &suites,
+                      bool verbose, std::vector<std::string> &parents) {
+    using namespace term;
+    const std::string indent(parents.size() * 2, ' ');
+
+    for(auto &suite : suites) {
+      parents.push_back(suite.name());
+
+      if(verbose) {
+        std::cout << indent << format(sgr::bold) << suite.name()
+                  << reset() << std::endl;
+      }
+
+      for(auto &test : suite) {
+        results.total++;
+
+        if(verbose)
+          std::cout << indent << "  " << test.name << " " << std::flush;
+        if(test.skip) {
+          results.skips++;
+          if(verbose) {
+            std::cout << format(sgr::bold, fg(color::blue)) << "SKIPPED"
+                      << reset() << std::endl;
+          }
+          continue;
+        }
+
+        auto result = test.function();
+        if(result.passed) {
+          results.passes++;
+          if(verbose) {
+            std::cout << format(sgr::bold, fg(color::green)) << "PASSED"
+                      << reset() << std::endl;
+          }
+        }
+        else {
+          results.failures.push_back({parents, test.name, result.message});
+          if(verbose) {
+            std::cout << format(sgr::bold, fg(color::red)) << "FAILED"
+                      << reset() << ": " << result.message << std::endl;
+          }
+        }
+      }
+
+      if(verbose)
+        std::cout << std::endl;
+
+      run_tests_impl(results, suite.subsuites(), verbose, parents);
+      parents.pop_back();
+    }
+  }
+
+  void run_tests(test_results &results, bool verbose) {
+    std::vector<std::string> parents;
+    run_tests_impl(results, all_suites, verbose, parents);
+  }
+}
+
 template<typename ...T, typename F>
 inline auto make_suite(const std::string &name, F &&f) {
   return make_basic_suite<expectation_error, T..., F>(name, std::forward<F>(f));
 }
-
-using suites_list = std::vector<runnable_suite>;
-suites_list all_suites;
 
 template<typename Exception, typename ...T>
 struct basic_suite {
 public:
   template<typename F>
   basic_suite(const std::string &name, F &&f,
-              suites_list &suites = all_suites) {
+              suites_list &suites = detail::all_suites) {
     suites.push_back(
       make_basic_suite<Exception, T...>(name, std::forward<F>(f))
     );
@@ -31,69 +114,11 @@ public:
 template<typename ...T>
 using suite = basic_suite<expectation_error, T...>;
 
-struct test_results {
-  test_results() : passes(0), skips(0), total(0) {}
-
-  struct failure {
-    std::string suite, test, message;
-  };
-  std::vector<failure> failures;
-  size_t passes, skips, total;
-};
-
-void run_tests(test_results &results, const suites_list &suites, bool verbose,
-               size_t depth = 0) {
-  using namespace term;
-  const std::string indent(depth * 2, ' ');
-
-  for(auto &suite : suites) {
-    if(verbose) {
-      std::cout << indent << format(sgr::bold) << suite.name()
-                << reset() << std::endl;
-    }
-
-    for(auto &test : suite) {
-      results.total++;
-
-      if(verbose)
-        std::cout << indent << "  " << test.name << " " << std::flush;
-      if(test.skip) {
-        results.skips++;
-        if(verbose) {
-          std::cout << format(sgr::bold, fg(color::blue)) << "SKIPPED"
-                    << reset() << std::endl;
-        }
-        continue;
-      }
-
-      auto result = test.function();
-      if(result.passed) {
-        results.passes++;
-        if(verbose) {
-          std::cout << format(sgr::bold, fg(color::green)) << "PASSED"
-                    << reset() << std::endl;
-        }
-      }
-      else {
-        results.failures.push_back({suite.name(), test.name, result.message});
-        if(verbose) {
-          std::cout << format(sgr::bold, fg(color::red)) << "FAILED"
-                    << reset() << ": " << result.message << std::endl;
-        }
-      }
-    }
-
-    if(verbose)
-      std::cout << std::endl;
-
-    run_tests(results, suite.subsuites(), verbose, depth + 1);
-  }
-}
-
 } // namespace mettle
 
 int main(int argc, const char *argv[]) {
   using namespace term;
+  using namespace mettle::detail;
   namespace opts = boost::program_options;
 
   opts::options_description desc("Allowed options");
@@ -114,9 +139,9 @@ int main(int argc, const char *argv[]) {
 
   colors_enabled = args.count("color");
   bool verbose = args.count("verbose");
-  mettle::test_results results;
 
-  run_tests(results, mettle::all_suites, verbose);
+  test_results results;
+  run_tests(results, verbose);
 
   std::cout << format(sgr::bold) << results.passes << "/" << results.total
             << " tests passed";
@@ -125,9 +150,9 @@ int main(int argc, const char *argv[]) {
   std::cout << reset() << std::endl;
 
   for(auto &i : results.failures) {
-    std::cout << "  " << i.suite << " > " << i.test << " "
-              << format(sgr::bold, fg(color::red)) << "FAILED" << reset()
-              << ": " << i.message << std::endl;
+    std::cout << "  " << join(i.suites.begin(), i.suites.end(), " > ")
+              << " > " << i.test << " " << format(sgr::bold, fg(color::red))
+              << "FAILED" << reset() << ": " << i.message << std::endl;
   }
 
   return results.failures.size();
