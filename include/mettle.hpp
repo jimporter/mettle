a4954c27b3d1a730753ad78b96b5e3e566f47b2f
Jim Porter <jporter@mozilla.com>
2014-04-11 02:32:08 -0500

    Remove old workaround for libstdc++

--- include/mettle.hpp
+++ include/mettle.hpp
@@ -1,8 +1,6 @@
 #ifndef INC_METTLE_HPP
 #define INC_METTLE_HPP
 
-extern "C" char* gets(char*); // Fix an issue with cstdio
-
 #include "mettle/suite.hpp"
 #include "mettle/matchers.hpp"
 #include "mettle/runner.hpp"
