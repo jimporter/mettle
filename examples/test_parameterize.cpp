8ad5f6b6a32e1c335b22920840448a49c5e457ac
Jim Porter <jporter@mozilla.com>
2014-06-15 19:12:38 -0500

    Make a better example for type-parameterized tests

--- examples/test_parameterize.cpp
+++ examples/test_parameterize.cpp
@@ -1,24 +1,49 @@
 #include <mettle.hpp>
 using namespace mettle;
 
-suite<int, float> param("parameterized suite", [](auto &_) {
+#include <vector>
+#include <list>
 
-  _.test("a test", [](auto &fixture) {
-    expect(fixture, equal_to(0));
-  });
+suite<std::vector<int>, std::list<int>> param("test collections", [](auto &_) {
 
-  subsuite<bool>(_, "a subsuite", [](auto &_) {
-    _.test("another test", [](auto &fixture1, bool &fixture2) {
-      expect(fixture1 && fixture2, equal_to(false));
+  subsuite<>(_, "empty collection", [](auto &_) {
+    _.test("size()", [](auto &coll) {
+      expect(coll.size(), equal_to(0u));
+    });
+
+    _.test("empty()", [](auto &coll) {
+      expect(coll.empty(), equal_to(true));
+    });
+
+    _.test("begin() == end()", [](auto &coll) {
+      expect(coll.begin(), equal_to(coll.end()));
     });
   });
 
-});
+  subsuite<>(_, "non-empty collection", [](auto &_) {
+    _.setup([](auto &coll) {
+      coll.push_back(1);
+      coll.push_back(0);
+      coll.push_back(2);
+    });
 
-suite<> fix("fixture suite", [](auto &_) {
+    _.test("size()", [](auto &coll) {
+      expect(coll.size(), equal_to(3u));
+    });
 
-  subsuite<int, float>(_, "blah", [](auto &_) {
-    _.test("my test", [](auto &) {
+    _.test("empty()", [](auto &coll) {
+      expect(coll.empty(), equal_to(false));
+    });
+  });
+
+  subsuite<int, float>(_, "emplacement", [](auto &_) {
+    _.setup([](auto &, auto &elt) {
+      elt = 1;
+    });
+
+    _.test("emplace_back()", [](auto &coll, auto &elt) {
+      coll.emplace_back(elt);
+      expect(coll.back(), equal_to(1));
     });
   });
 
