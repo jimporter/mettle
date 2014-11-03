8761d36088bb9e0b7eeac9a8c31dddeecdc8457a
Jim Porter <jporter@mozilla.com>
2014-11-03 01:21:12 -0600

    Be extra-careful about closing pipes; don't close stdout/stderr by mistake!

--- src/libmettle/forked_test_runner.cpp
+++ src/libmettle/forked_test_runner.cpp
@@ -56,13 +56,19 @@ test_result forked_test_runner::operator ()(
        log_pipe.close_read() < 0)
       child_failed();
 
-    if(dup2(stdout_pipe.write_fd, STDOUT_FILENO) < 0 ||
-       dup2(stderr_pipe.write_fd, STDERR_FILENO) < 0)
-      child_failed();
+    if(stdout_pipe.write_fd != STDOUT_FILENO) {
+      if(dup2(stdout_pipe.write_fd, STDOUT_FILENO) < 0)
+        child_failed();
+      if(stdout_pipe.close_write() < 0)
+        child_failed();
+    }
 
-    if(stdout_pipe.close_write() < 0 ||
-       stderr_pipe.close_write() < 0)
-      child_failed();
+    if(stderr_pipe.write_fd != STDERR_FILENO) {
+      if(dup2(stderr_pipe.write_fd, STDERR_FILENO) < 0)
+        child_failed();
+      if(stderr_pipe.close_write() < 0)
+        child_failed();
+    }
 
     auto result = test();
     if(write(log_pipe.write_fd, result.message.c_str(),
