4d34601131941f42338a42b98616f0024a9dc3a2
Jim Porter <jporter@mozilla.com>
2014-11-19 00:03:41 -0600

    Fix some race conditions in fork_monitor, and also be paranoid about other failures

--- src/libmettle/test_monitor.cpp
+++ src/libmettle/test_monitor.cpp
@@ -10,7 +10,11 @@
 namespace mettle {
 
 namespace {
-  [[noreturn]] inline void child_failed() {
+  [[noreturn]] void monitor_failed(int pid1 = -1, int pid2 = -1) {
+    if(pid1 != -1)
+      kill(pid1, SIGKILL);
+    if(pid2 != -1)
+      kill(pid2, SIGKILL);
     _exit(128);
   }
 }
@@ -19,38 +23,60 @@ void fork_monitor(std::chrono::milliseconds timeout) {
   // Assume our process group is different from our parent's. However, we want
   // some of our processes to rejoin the parent's process group.
   pid_t parent_pgid = getpgid(getppid());
-  assert(parent_pgid != getpgid(0));
+  pid_t test_pgid = getpgid(0);
+  assert(parent_pgid != test_pgid);
 
   pid_t timer_pid;
   if((timer_pid = fork()) < 0)
-    child_failed();
+    monitor_failed();
   if(timer_pid == 0) {
-    setpgid(0, parent_pgid);
+    if(setpgid(0, parent_pgid) < 0)
+      monitor_failed();
     std::this_thread::sleep_for(timeout);
     _exit(err_timeout);
   }
 
+  if(setpgid(timer_pid, parent_pgid) < 0)
+    monitor_failed(timer_pid);
+
+  // The monitor process will signal the test process when it's ok to proceed
+  // (i.e. once it's rejoined the parent's process group). Set up a signal mask
+  // to wait for it.
+  sigset_t mask, oldmask;
+  sigemptyset(&mask);
+  sigaddset(&mask, SIGUSR1);
+  sigprocmask(SIG_BLOCK, &mask, &oldmask);
+
   pid_t test_pid;
-  if((test_pid = fork()) < 0) {
-    kill(timer_pid, SIGKILL);
-    child_failed();
-  }
+  if((test_pid = fork()) < 0)
+    monitor_failed(timer_pid);
   if(test_pid != 0) {
-    pid_t test_pgid = getpgid(0);
-    setpgid(0, parent_pgid);
+    sigprocmask(SIG_SETMASK, &oldmask, nullptr);
+    if(setpgid(0, parent_pgid) < 0 ||
+       kill(test_pid, SIGUSR1) < 0)
+      monitor_failed(timer_pid, test_pid);
 
     // Wait for the first child process (the timer or the test) to finish,
     // then kill (and wait) for the other one. If the timer finishes first, kill
     // the test's entire process group.
     int status;
-    pid_t exited_pid = wait(&status);
-    kill(exited_pid == test_pid ? timer_pid : -test_pgid, SIGKILL);
+    pid_t exited_pid;
+    if((exited_pid = wait(&status)) < 0)
+      monitor_failed(timer_pid, -test_pgid);
+    if(kill(exited_pid == test_pid ? timer_pid : -test_pgid, SIGKILL) < 0)
+      monitor_failed(timer_pid, -test_pgid);
     wait(nullptr);
 
     if(WIFEXITED(status))
       _exit(WEXITSTATUS(status));
     else // WIFSIGNALED
       raise(WTERMSIG(status));
+  }
+  else {
+    int sig;
+    if(sigwait(&mask, &sig) < 0)
+      monitor_failed();
+    sigprocmask(SIG_SETMASK, &oldmask, nullptr);
   }
 }
 
