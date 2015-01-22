ef4d92fffc9ca4197fdda20cc0daf4b46f81d0b0
Jim Porter <jporter@mozilla.com>
2015-01-22 15:00:57 -0600

    Use our constexpr max_() in glue.hpp too

--- include/mettle/glue.hpp
+++ include/mettle/glue.hpp
@@ -37,7 +37,7 @@ make_suite(const std::string &name, F &&f) {
 }
 
 template<typename ...Fixture, typename Factory, typename F>
-inline std::array<runnable_suite, std::max<size_t>(sizeof...(Fixture), 1)>
+inline std::array<runnable_suite, detail::max_(sizeof...(Fixture), 1)>
 make_suites(const std::string &name, const attributes &attrs,
            Factory &&factory, F &&f) {
   return make_basic_suites<expectation_error, Fixture...>(
@@ -46,7 +46,7 @@ make_suites(const std::string &name, const attributes &attrs,
 }
 
 template<typename ...Fixture, typename Factory, typename F>
-inline std::array<runnable_suite, std::max<size_t>(sizeof...(Fixture), 1)>
+inline std::array<runnable_suite, detail::max_(sizeof...(Fixture), 1)>
 make_suites(const std::string &name, Factory &&factory, F &&f) {
   return make_basic_suites<expectation_error, Fixture...>(
     name, std::forward<Factory>(factory), std::forward<F>(f)
@@ -54,13 +54,13 @@ make_suites(const std::string &name, Factory &&factory, F &&f) {
 }
 
 template<typename ...Fixture, typename F>
-inline std::array<runnable_suite, std::max<size_t>(sizeof...(Fixture), 1)>
+inline std::array<runnable_suite, detail::max_(sizeof...(Fixture), 1)>
 make_suites(const std::string &name, const attributes &attrs, F &&f) {
   return make_suites<Fixture...>(name, attrs, auto_factory, std::forward<F>(f));
 }
 
 template<typename ...Fixture, typename F>
-inline std::array<runnable_suite, std::max<size_t>(sizeof...(Fixture), 1)>
+inline std::array<runnable_suite, detail::max_(sizeof...(Fixture), 1)>
 make_suites(const std::string &name, F &&f) {
   return make_suites<Fixture...>(name, auto_factory, std::forward<F>(f));
 }
