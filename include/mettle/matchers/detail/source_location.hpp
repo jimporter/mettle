04677d3761b248e4de1082763452d587487ece1b
Jim Porter <itsjimporter@gmail.com>
2019-12-05 16:22:35 -0800

    We can always use __has_include now

--- include/mettle/matchers/detail/source_location.hpp
+++ include/mettle/matchers/detail/source_location.hpp
@@ -7,7 +7,7 @@
 #if defined(METTLE_USE_STDLIB_EXTS)
 #  include <experimental/source_location>
 #  define METTLE_SOURCE_LOCATION std::experimental::source_location
-#elif !defined(METTLE_NO_STDLIB_EXTS) && defined(__has_include)
+#else
    // XXX: clang probably won't support libstdc++'s source_location, since it
    // lacks the required compiler magic. Just disable it for now. See
    // <https://gcc.gnu.org/ml/gcc-patches/2015-11/msg01687.html>.
@@ -19,9 +19,6 @@
 #    include "source_location_shim.hpp"
 #    define METTLE_SOURCE_LOCATION mettle::detail::source_location
 #  endif
-#else
-#  include "source_location_shim.hpp"
-#  define METTLE_SOURCE_LOCATION mettle::detail::source_location
 #endif
 
 #endif
