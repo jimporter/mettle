6b0a3aadd4c099cc19eb456c91ac869373fbfa31
Jim Porter <itsjimporter@gmail.com>
2021-03-19 19:05:07 -0700

    Improve support detection for std::source_location

--- include/mettle/matchers/detail/source_location.hpp
+++ include/mettle/matchers/detail/source_location.hpp
@@ -1,18 +1,20 @@
 #ifndef INC_METTLE_MATCHERS_DETAIL_SOURCE_LOCATION_HPP
 #define INC_METTLE_MATCHERS_DETAIL_SOURCE_LOCATION_HPP
 
-// Get _LIBCPP_VERSION to detect libc++.
-#include <ciso646>
+// Older versions of clang (8 and below) don't have `__builtin_FILE` and so
+// won't work with `std[::experimental]::source_location`.
+#if defined(__clang__)
+#  if !__has_builtin(__builtin_FILE)
+#    include "source_location_shim.hpp"
+#    define METTLE_SOURCE_LOCATION mettle::detail::source_location
+#  endif
+#endif
 
-#if defined(METTLE_USE_STDLIB_EXTS)
-#  include <experimental/source_location>
-#  define METTLE_SOURCE_LOCATION std::experimental::source_location
-#else
-   // XXX: clang probably won't support libstdc++'s source_location, since it
-   // lacks the required compiler magic. Just disable it for now. See
-   // <https://gcc.gnu.org/ml/gcc-patches/2015-11/msg01687.html>.
-#  if !(defined(__clang__) && !defined(_LIBCPP_VERSION) && \
-        defined(__GLIBCXX__)) && __has_include(<experimental/source_location>)
+#ifndef METTLE_SOURCE_LOCATION
+#  if __has_include(<source_location>)
+#    include <source_location>
+#    define METTLE_SOURCE_LOCATION std::source_location
+#  elif __has_include(<experimental/source_location>)
 #    include <experimental/source_location>
 #    define METTLE_SOURCE_LOCATION std::experimental::source_location
 #  else
