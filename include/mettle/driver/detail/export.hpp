e30b20aaaa9c2120c72305e36665892437bab3b7
Jim Porter <itsjimporter@gmail.com>
2025-09-08 17:44:47 -0700

    s/METTLE_STATIC/LIBMETTLE_STATIC/g
    This is the correct naming for the bfg9000 static-library macro.

--- include/mettle/driver/detail/export.hpp
+++ include/mettle/driver/detail/export.hpp
@@ -1,7 +1,7 @@
 #ifndef INC_METTLE_DRIVER_DETAIL_EXPORT_HPP
 #define INC_METTLE_DRIVER_DETAIL_EXPORT_HPP
 
-#if defined(_WIN32) && !defined(METTLE_STATIC)
+#if defined(_WIN32) && !defined(LIBMETTLE_STATIC)
 #  ifdef LIBMETTLE_EXPORTS
 #    define METTLE_PUBLIC __declspec(dllexport)
 #  else
