4b05638daa02d0870de25d4393c00109196af638
Jim Porter <itsjimporter@gmail.com>
2025-12-13 08:49:10 -0800

    Simplify conditional logic for library export macros

--- include/mettle/driver/detail/export.hpp
+++ include/mettle/driver/detail/export.hpp
@@ -4,18 +4,16 @@
 #ifdef LIBMETTLE_STATIC
 #  define METTLE_PUBLIC
 #  define METTLE_PUBLIC_FRIEND
-#else
-#  ifdef _WIN32
-#    ifdef LIBMETTLE_EXPORTS
-#      define METTLE_PUBLIC __declspec(dllexport)
-#    else
-#      define METTLE_PUBLIC __declspec(dllimport)
-#    endif
-#    define METTLE_PUBLIC_FRIEND METTLE_PUBLIC
+#elif defined(_WIN32)
+#  ifdef LIBMETTLE_EXPORTS
+#    define METTLE_PUBLIC __declspec(dllexport)
 #  else
-#    define METTLE_PUBLIC [[gnu::visibility("default")]]
-#    define METTLE_PUBLIC_FRIEND
+#    define METTLE_PUBLIC __declspec(dllimport)
 #  endif
+#  define METTLE_PUBLIC_FRIEND METTLE_PUBLIC
+#else
+#  define METTLE_PUBLIC [[gnu::visibility("default")]]
+#  define METTLE_PUBLIC_FRIEND
 #endif
 
 #endif
